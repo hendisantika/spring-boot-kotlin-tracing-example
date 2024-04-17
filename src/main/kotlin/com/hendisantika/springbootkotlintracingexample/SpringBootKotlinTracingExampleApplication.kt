3d0e7bf05630e541b93c7e6925dcbdbcc5baf6bd
hendisantika <hendisantika@yahoo.co.id>
2024-04-17 10:08:52 +0700

    Add TodoController🫸🌀✏️📗🐧🐳

--- src/main/kotlin/com/hendisantika/springbootkotlintracingexample/SpringBootKotlinTracingExampleApplication.kt
+++ src/main/kotlin/com/hendisantika/springbootkotlintracingexample/SpringBootKotlinTracingExampleApplication.kt
@@ -11,6 +11,8 @@ import org.springframework.boot.runApplication
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 
 @SpringBootApplication
@@ -59,3 +61,10 @@ suspend fun runObserved(name: String, observationRegistry: ObservationRegistry, 
         }
     }.awaitSingleOrNull()
 }
+
+@RestController
+class TodoController(
+    val observationRegistry: ObservationRegistry,
+    val todoRepo: ToDoRepository,
+    webClientBuilder: WebClient.Builder
+)
