95f202ebcbe4c90687e0de3f0a53db596abf17bd
hendisantika <hendisantika@yahoo.co.id>
2024-04-17 10:03:12 +0700

    Upgraded into Spring Boot 3.2.0 version🫸🌀✏️📗🐧🐳

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val micrometer_tracing = "1.0.0"
 
 plugins {
-    id("org.springframework.boot") version "3.1.9"
+    id("org.springframework.boot") version "3.2.0"
     id("io.spring.dependency-management") version "1.1.3"
     kotlin("jvm") version "1.9.20-Beta2"
     kotlin("plugin.spring") version "1.9.20-Beta2"
