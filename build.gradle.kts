022b734dcc2948d3b6ebddd1d7f36d0d11646136
Hendi Santika <hendisantika@yahoo.co.id>
2024-09-23 06:19:38 +0700

    Upgraded into Spring Boot 3.3.0 version 🫸🌀✏️📗🐧🐳⬆
    Upgraded into Spring Boot 3.3.0 version 🫸🌀✏️📗🐧🐳⬆

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val micrometer_tracing = "1.0.0"
 
 plugins {
-    id("org.springframework.boot") version "3.2.9"
+    id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
